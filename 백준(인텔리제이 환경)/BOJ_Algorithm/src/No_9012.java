f8c85465b1b689a814734e33d45af08adbe53c5e
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-10 13:33:13 +0900

    2022-10-10

---
+++ 백준(인텔리제이 환경)/BOJ_Algorithm/src/No_9012.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class No_9012 {
+    public static void main(String[] args) throws IOException {
+        //괄호의 갯수가 짝이 맞는 가? && ) 가 먼저 들어오지 않는가?
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.valueOf(br.readLine());
+        for (int i = 1; i <= N; i++) {
+            if (isVPS(br.readLine())) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+
+    static boolean isVPS(String input) {
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                result++;
+            } else if (input.charAt(i) == ')') {
+                result--;
+            }
+            if (result == -1) {
+                return false;
+            }
+        }
+        return result == 0;
+    }
+}
