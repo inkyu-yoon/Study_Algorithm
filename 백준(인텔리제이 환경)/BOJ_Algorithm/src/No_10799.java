75cecf5ac52360b9df16e3229befa66e61398f49
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-10 14:29:19 +0900

    2022-10-10

---
+++ 백준(인텔리제이 환경)/BOJ_Algorithm/src/No_10799.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class No_10799 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+
+        Stack<Character> stack = new Stack<>();
+
+        int result = 0;
+        for (int i = 0; i < input.length(); i++) {
+            if (input.charAt(i) == '(') {
+                stack.push('(');
+            }
+
+            if (input.charAt(i) == ')') {
+                if (input.charAt(i - 1) == '(') {
+                    stack.pop();
+                    result += stack.size();
+                } else {
+                    stack.pop();
+                    result += 1;
+                }
+            }
+        }
+        System.out.println(result);
+    }
+}
