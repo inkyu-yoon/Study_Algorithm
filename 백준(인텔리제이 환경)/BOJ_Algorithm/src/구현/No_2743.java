8b0a49b602506ad9fb17a67d015274b00bc83465
inkyu-yoon <tryingmybest24h@gmail.com>
2022-10-12 14:30:26 +0900

    2022-10-12 2743 [단어 길이 재기]

---
+++ 백준(인텔리제이 환경)/BOJ_Algorithm/src/구현/No_2743.java
@@ -0,0 +1,13 @@
+package 구현;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No_2743{
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
+        System.out.println(input.length());
+    }
+}
