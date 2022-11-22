c2d956a5967ef43e99f931ea075d67fe52d50d61
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-22 17:30:46 +0900

    ADD: 2022-11-22 해시 【완주하지 못한 선수】

--- Programmers/src/해시/완주하지못한선수/Solution.java
+++ Programmers/src/해시/완주하지못한선수/Solution.java
@@ -2,35 +2,36 @@ package 해시.완주하지못한선수;
 
 import java.util.*;
 
-//완주하지 못한 선수의 이름을 return
-//단 한명만 완주를 못함
-//completion의 길이는 participant의 길이보다 1 작음
-//participant에 동명이인이 있을 수 있음
 class Solution {
+    // 단 한명의 선수를 제외하고 모든 선수가 마라톤 완주
+    // completion의 길이는 participant의 길이보다 1 작음
+    // 동명이인이 있을 수 있음
+
     public String solution(String[] participant, String[] completion) {
         String answer = "";
-        Map<String,Integer> check = new HashMap<>();
+        Map<String, Integer> check = new HashMap<>();
 
-        //참석자 입력
-        for(String input : participant){
-            if(check.containsKey(input)){
-                check.put(input,check.get(input)+1);
-            }else{
-                check.put(input,1);
+        //참가자를 먼저 map에 등록(동명이인이 있을 수 있음)
+        for (String person : participant) {
+            if (check.containsKey(person)) {
+                check.put(person, check.get(person) + 1);
+            } else {
+                check.put(person, 1);
             }
         }
 
-        //완주자 value 값 -1씩 하기
-        for(String finish : completion){
-            check.put(finish,check.get(finish)-1);
+        // 완주한 사람 이름은 value값을 -1씩 한다.
+        for (String finish : completion) {
+            check.put(finish, check.get(finish) - 1);
         }
 
-        // 최종 value 값이 1인 사람이 완주를 못한 사람일 것
-        for(String key : check.keySet()){
-            if(check.get(key)==1){
-                answer = key;
+        // 한 명만 완주를 못했으므로, value 값이 여전히 1인 사람이 완주를 못한 것
+        for (String person : check.keySet()) {
+            if (check.get(person) == 1) {
+                answer = person;
             }
         }
+
 
         return answer;
     }
