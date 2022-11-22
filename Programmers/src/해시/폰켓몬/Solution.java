7dccfb163abad29abba1079bd86fcf620e5ac83b
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-11-22 17:48:41 +0900

    ADD: 2022-11-22 해시 【폰켓몬】

--- Programmers/src/해시/폰켓몬/Solution.java
+++ Programmers/src/해시/폰켓몬/Solution.java
@@ -1,29 +1,32 @@
 package 해시.폰켓몬;
 
 import java.util.*;
-class Solution {
-    // N 마리의 폰켓몬 중 N/2마리를 가져갈 수 있다.
-    // 예시 [3 1 2 3] 일 때,
-    // [3 1] [3 2] [1 2] 를 가져갈 때, 가장 다양한 종류의 폰켓몬을 가져갈 수 있다.
 
+class Solution {
+    // 총 N마리의 폰켓몬 중 N/2 마리를 가져갈 수 있다.
+    // N/2 마리를 고르는 과정에서 가장 다양한 종류로 가져갈 수 있는 최댓값을 구한다.
+    // 중복 제거가 필요하다.
     public int solution(int[] nums) {
         int answer = 0;
+        Map<Integer, Integer> monsters = new HashMap<>();
 
-        //중복을 제거해야한다.
-        Map<Integer,Integer> mon = new HashMap<>();
-        for(int num : nums){
-            mon.put(num,1);
+        // 가져갈 수 있는 폰켓몬 수
+        int getable = nums.length / 2;
+
+        //맵에 저장하면서 중복 제거
+        for (int num : nums) {
+            monsters.put(num, 1);
         }
 
-        //고를 수 있는 폰켓몬 종류를 얻는다.
-        int size = mon.keySet().size();
+        // 폰켓몬 종류의 개수
+        int species = monsters.size();
 
-        //폰켓몬수/2 보다 폰켓몬 종류가 적다면, 폰켓몬 종류의 수가 곧 최댓값
-        if(size<=nums.length/2){
-            answer = size;
-            // 폰켓몬 종류가 폰켓몬 수/2 보다 크다면, 폰켓몬 수/2 가 최댓값
-        }else{
-            answer = nums.length/2;
+        // 가져갈 수 있는 폰켓몬 수 >= 폰켓몬 종류 인 경우, 폰켓몬 종류 갯수가 최댓값
+        if (getable >= species) {
+            answer = species;
+            // 가져갈 수 있는 폰켓몬 수 < 폰켓몬 종류 인 경우, 종류가 아무리 다양해도 가져갈 수 있는 갯수의 한계가 있음
+        } else {
+            answer = getable;
         }
 
         return answer;
