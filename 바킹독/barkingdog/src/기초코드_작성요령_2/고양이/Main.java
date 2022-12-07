9c91bad9e96d1e023a4312cc5b27f837ebe81088
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2022-12-07 23:58:37 +0900

    ADD : 22.12.07 기초 코드 작성 요령 2 - 고양이

---
+++ 바킹독/barkingdog/src/기초코드_작성요령_2/고양이/Main.java
@@ -0,0 +1,15 @@
+package 기초코드_작성요령_2.고양이;
+
+public class Main {
+    public static void main(String[] args) {
+//\    /\
+// )  ( ')
+//(  /  )
+// \(__)|
+        System.out.println("\\    /\\");
+        System.out.println(" )  ( ')");
+        System.out.println("(  /  )");
+        System.out.println(" \\(__)|");
+
+    }
+}
