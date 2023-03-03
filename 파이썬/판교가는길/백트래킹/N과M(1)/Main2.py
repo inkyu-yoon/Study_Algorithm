567ff3999554262f345b4a73e703362f80e5448a
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-03 12:41:57 +0900

    ADD: 2022-03-03 백트래킹 【N과M(1)】 복습

---
+++ 파이썬/판교가는길/백트래킹/N과M(1)/Main2.py
@@ -0,0 +1,20 @@
+def dfs():
+    if len(stack) == M:
+        print(' '.join(map(str, stack)))
+
+    for i in range(1, N+1):
+        if checked[i] == True:
+            continue
+        stack.append(i)
+        checked[i] = True
+        dfs()
+        stack.pop()
+        checked[i] = False
+
+N,M = map(int,input().split())
+
+stack = []
+checked = [False]*(N+1)
+
+dfs()
+
