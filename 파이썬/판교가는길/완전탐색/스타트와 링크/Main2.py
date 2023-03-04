c1a2d731ca3ace2f7d4522f7876bd6b65f5e3551
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-04 21:40:50 +0900

    ADD: 2022-03-04 백트래킹 【스타트와 링크】

--- 파이썬/판교가는길/완전탐색/스타트와 링크/Main2.py
+++ 파이썬/판교가는길/완전탐색/스타트와 링크/Main2.py
@@ -1,9 +1,43 @@
 import sys
 
 N = int(input())
-stat = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 
-checked = [False] * N
+board = [list(map(int, sys.stdin.readline().split())) for _ in range(N)]
 
-_min = 1e9
+stack = []
+checked = [False] * (N + 1)
 
+ans = 1e9
+
+
+def bt():
+    global ans
+
+    if len(stack) == N // 2:
+        startScore = 0
+        linkScore = 0
+        for i in range(1, N + 1):
+            for j in range(1, N + 1):
+                if checked[i] and checked[j] and i < j:
+                    startScore += board[i - 1][j - 1] + board[j - 1][i - 1]
+                if not checked[i] and not checked[j] and i < j:
+                    linkScore += board[i - 1][j - 1] + board[j - 1][i - 1]
+        ans = min(abs(startScore - linkScore), ans)
+
+        return
+
+    for i in range(1, N + 1):
+        if checked[i] == True:
+            continue
+        if len(stack) >= 1 and stack[-1] > i:
+            continue
+
+        stack.append(i)
+        checked[i] = True
+        bt()
+        stack.pop()
+        checked[i] = False
+
+
+bt()
+print(ans)
