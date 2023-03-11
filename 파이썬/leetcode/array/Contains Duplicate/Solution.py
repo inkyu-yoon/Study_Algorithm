d214c2cdc5ef4979b442588144a082e7a40e7016
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-11 15:33:06 +0900

    ADD: 2022-03-11 Array 【Contains Duplicate】

---
+++ 파이썬/leetcode/array/Contains Duplicate/Solution.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        dic = {}
+
+        for i in range(len(nums)):
+
+            if nums[i] in dic:
+                return True
+
+            dic[nums[i]] = i
+
