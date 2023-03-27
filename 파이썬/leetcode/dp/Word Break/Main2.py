bd1ded6c9eeb1f62da6dbf292efdaaa4cf48ef80
Yoon In Kyu <110657129+inkyu-yoon@users.noreply.github.com>
2023-03-27 10:49:00 +0900

    ADD: 2022-03-27 LeetCode 【Word Break】 복습

---
+++ 파이썬/leetcode/dp/Word Break/Main2.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def wordBreak(self, s, wordDict):
+        dp=[False for _ in range(len(s))]
+        for i in range(len(s)):
+            if s[:i+1] in wordDict:
+                dp[i]=True
+            for j in range(i):
+                if dp[j] and s[j+1:i+1] in wordDict:
+                    dp[i]=True
+        return dp[-1]
