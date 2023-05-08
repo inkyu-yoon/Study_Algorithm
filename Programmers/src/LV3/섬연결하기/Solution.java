485f104ff5a9ae29d605058ef2b5d0d05b8d7cb3
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-08 10:21:08 +0900

    ADD: 2022-05-08 프로그래머스 【LV3.섬 연결하기】

---
+++ Programmers/src/LV3/섬연결하기/Solution.java
@@ -0,0 +1,46 @@
+package LV3.섬연결하기;
+
+
+import java.util.*;
+class Solution {
+    int[] parent;
+    public int solution(int n, int[][] costs) {
+        int answer = 0;
+
+        setUp(n);
+
+        Arrays.sort(costs,(a,b)->a[2]-b[2]);
+
+        for(int[] cost : costs){
+            if(find(cost[0])!=find(cost[1])){
+                union(cost[0],cost[1]);
+                answer+=cost[2];
+            }
+        }
+
+        return answer;
+    }
+
+    void union(int num1, int num2){
+        int a = find(num1);
+        int b = find(num2);
+        if(a!=b){
+            parent[b]=a;
+        }
+    }
+
+    int find(int num){
+        if(parent[num]==num){
+            return num;
+        }else{
+            return find(parent[num]);
+        }
+    }
+
+    void setUp(int n){
+        parent = new int[n];
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+}
