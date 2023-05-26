f179fc1d116fbd0cb3444331c86b9e53b5a50867
inkyu-yoon <tryingmybest24h@gmail.com>
2023-05-26 11:06:06 +0900

    ADD: 2022-05-26 백준 1463 【S3.1로 만들기】

---
+++ Programmers/src/판교가는길/dp/일로만들기/Main.java
@@ -0,0 +1,37 @@
+package 판교가는길.dp.일로만들기;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+
+public class Main {
+    final int MAX = 1000001;
+    public static void main(String[] args) throws IOException {
+        new Main().solution();
+    }
+
+    void solution() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] dp = new int[N + 1];
+        Arrays.fill(dp, MAX);
+        dp[0]=0;
+        dp[1]=0;
+        for (int i = 2; i <= N; i++) {
+            dp[i] = dp[i - 1] + 1;
+            if (i % 2 == 0) {
+                dp[i] = Math.min(dp[i / 2] + 1, dp[i]);
+            }
+            if (i % 3 == 0) {
+                dp[i] = Math.min(dp[i / 3] + 1, dp[i]);
+            }
+        }
+
+        System.out.println(Arrays.toString(dp));
+
+
+
+    }
+}
