71acefe35898830edeca289af29f5c28af005f2c
inkyu-yoon <tryingmybest24h@gmail.com>
2023-11-16 10:49:06 +0900

    프로그래머스 【LV2.124 나라의 숫자】

---
+++ Programmers/src/LV2/일이사나라의숫자/Solution2.java
@@ -0,0 +1,21 @@
+package LV2.일이사나라의숫자;
+
+class Solution2 {
+    public String solution(int n) {
+        return convert(n).replaceAll("3", "4");
+    }
+
+    String convert(int n) {
+        StringBuilder sb = new StringBuilder();
+        while (n != 0) {
+            if (n % 3 == 0) {
+                sb.append("3");
+                n = n / 3 - 1;
+            } else {
+                sb.append(n % 3);
+                n /= 3;
+            }
+        }
+        return sb.reverse().toString();
+    }
+}
